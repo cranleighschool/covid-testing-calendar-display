99defa8c701332c401c6198f7cd5c50666ca7089
Fred Bradley <hello@fredbradley.co.uk>
2021-03-03 14:27:02 +0000

    add in sanitization for cps year groups

--- src/SanitizeYearGroup.php
+++ src/SanitizeYearGroup.php
@@ -43,6 +43,24 @@ class SanitizeYearGroup
         if (preg_match('/^\A(9|10|11|12|13)\z$/', $str)) {
             $int = (int) $str;
             switch ($int) {
+                case 3:
+                    return "Form 1";
+                    break;
+                case 4:
+                    return "Form 2";
+                    break;
+                case 5:
+                    return "Form 3";
+                    break;
+                case 6:
+                    return "Form 4";
+                    break;
+                case 7:
+                    return "Form 5";
+                    break;
+                case 8:
+                    return "Form 6";
+                    break;
                 case 9:
                     return "IV Form";
                     break;
